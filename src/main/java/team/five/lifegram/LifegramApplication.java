f86c89d3d7320c52037b92f8ffbe99b227b6cca2
ironprayer <ironprayer1208@gmail.com>
2023-07-21 15:02:32 +0900

    init: init project

---
+++ src/main/java/team/five/lifegram/LifegramApplication.java
@@ -0,0 +1,13 @@
+package team.five.lifegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/five/lifegram/LifegramApplicationTests.java
@@ -0,0 +1,13 @@
+package team.five.lifegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
