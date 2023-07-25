557556e2981268a6192bc37a0dd538a1f6bc9143
ironprayer <ironprayer1208@gmail.com>
2023-07-25 14:56:21 +0900

    feat: jacoco setting

---
+++ src/test/java/team/five/lifegram/domain/post/service/PostServiceTest.java
@@ -0,0 +1,17 @@
+package team.five.lifegram.domain.post.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    @DisplayName("Jacoco Test")
+    void jacocoTest(){
+        System.out.println("Jacoco Test");
+    }
+
+}
