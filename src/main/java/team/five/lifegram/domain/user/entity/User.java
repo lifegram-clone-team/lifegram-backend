a43b81189193fc6054c9a56fce4ef644f6562e80
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-07-25 15:15:15 +0900

    chore: User update

--- src/main/java/team/five/lifegram/domain/user/entity/User.java
+++ src/main/java/team/five/lifegram/domain/user/entity/User.java
@@ -2,12 +2,9 @@ package team.five.lifegram.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CurrentTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import team.five.lifegram.domain.post.entity.Post;
 import team.five.lifegram.global.type.BaseTime;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
