10012d0ca1697734d481822c331a932f9812194e
ironprayer <ironprayer1208@gmail.com>
2023-07-26 19:00:06 +0900

    test: implement login

--- src/test/java/team/five/lifegram/domain/auth/service/AuthServiceTest.java
+++ src/test/java/team/five/lifegram/domain/auth/service/AuthServiceTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import team.five.lifegram.domain.auth.dto.LoginRequestDto;
 import team.five.lifegram.domain.auth.dto.SignupRequestDto;
+import team.five.lifegram.domain.auth.dto.TokenResponseDto;
 import team.five.lifegram.domain.user.entity.User;
 import team.five.lifegram.domain.user.repository.UserRepository;
-import team.five.lifegram.global.config.SecurityConfig;
 
 import java.util.Optional;
 
@@ -21,7 +22,8 @@ class AuthServiceTest {
 
     @Mock
     UserRepository userRepository;
-    PasswordEncoder passwordEncoder = new SecurityConfig().passwordEncoder();
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     @Test
     @DisplayName("회원가입 유저이름이 중복되는 경우 테스트")
@@ -59,10 +61,46 @@ class AuthServiceTest {
         given(userRepository.findByUserName(signupRequestDto.userName())).willReturn(Optional.empty());
         given(userRepository.findByEmail(signupRequestDto.email())).willReturn(Optional.empty());
 
-       assertDoesNotThrow(() -> authService.signup(signupRequestDto));
+        assertDoesNotThrow(() -> authService.signup(signupRequestDto));
     }
 
     @Test
-    void login() {
+    @DisplayName("로그인시 이메일 존재하지 않은 경우 테스트")
+    void loginNotEmailTest() {
+        AuthService authService = new AuthService(userRepository, passwordEncoder);
+        LoginRequestDto loginRequestDto = new LoginRequestDto("","");
+        given(userRepository.findByEmail(loginRequestDto.email())).willReturn(Optional.empty());
+
+        Exception e = assertThrows(IllegalArgumentException.class, () -> authService.login(loginRequestDto));
+
+        assertEquals(e.getMessage(), "이메일이 존재하지 않습니다");
+    }
+
+    @Test
+    @DisplayName("로그인시 비밀번호가 유효하지 않은 경우 테스트")
+    void loginInvalidPasswordTest() {
+        AuthService authService = new AuthService(userRepository, passwordEncoder);
+        LoginRequestDto loginRequestDto = new LoginRequestDto("","");
+        User user = User.builder().build();
+        given(userRepository.findByEmail(loginRequestDto.email())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(loginRequestDto.password(),user.getPassword())).willReturn(false);
+
+        Exception e = assertThrows(IllegalArgumentException.class, () -> authService.login(loginRequestDto));
+
+        assertEquals(e.getMessage(), "비밀번호가 맞지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    void loginSuccessTest() {
+        AuthService authService = new AuthService(userRepository, passwordEncoder);
+        LoginRequestDto loginRequestDto = new LoginRequestDto("","");
+        User user = User.builder().id(1L).build();
+        given(userRepository.findByEmail(loginRequestDto.email())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(loginRequestDto.password(),user.getPassword())).willReturn(true);
+
+        TokenResponseDto tokenResponseDto = authService.login(loginRequestDto);
+
+        assertNotNull(tokenResponseDto);
     }
 }
